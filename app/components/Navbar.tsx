b3ac9e1a9c2153a62fbd7bf65733b590a98cc7cf
Mohamed ezz <mohamedezz195@gmail.com>
2024-09-11 10:08:22 +0300

    home and nav

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return <DesktopNavbar />;
+};
+
+const DesktopNavbar = () => {
+  return (
+    <nav className="bg-gray-800">
+
+    </nav>
+  );
+};
+
+export default Navbar;
