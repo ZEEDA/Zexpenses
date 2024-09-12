1ef11ccd6cbc96b50095c9d6c44777fdda05c8e5
Mohamed ezz <mohamedezz195@gmail.com>
2024-09-12 04:12:53 +0300

    Add website header for desktop and mobilr

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -5,7 +5,9 @@ const Logo = () => {
   return (
     <a href="/" className="flex items-center gap-2">
       <BanknoteIcon className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">Zexpenses Tracker</p>
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        ZEXT
+      </p>
     </a>
   );
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,112 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Logo from "./Logo";
+import { link } from "fs";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { ThemeSwitcherBtn } from "./ThemeSwitcherBtn";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
-  return <DesktopNavbar />;
+  return (
+    <>
+      <DesktopNavbar />
+      <MobileNavbar />
+    </>
+  );
 };
-
+const items = [
+  { label: "Dashboard", link: "/" },
+  { label: "Transactions", link: "/transactions" },
+  { label: "Manage", link: "/manage" },
+];
+const MobileNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+              <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+                {items.map((item) => (
+                    <NavbarItem
+                        key={item.label}
+                        link={item.link}
+                        label={item.label}
+                        handleClick={() => setIsOpen(false)}
+                    />
+                ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+            <Logo />
+        </div>
+        <div className="flex items-center gap-2">
+            <ThemeSwitcherBtn />
+            <UserButton afterSwitchSessionUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+};
 const DesktopNavbar = () => {
   return (
-    <nav className="bg-gray-800">
-
-    </nav>
+    <div className="hidden border-separate border-b bg-background md:block">
+      <nav className="container flex items-center justify-between px-8 mx-auto">
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <Logo />
+          <div className="flex h-full">
+            {items.map((item) => (
+              <NavbarItem
+                key={item.label}
+                link={item.link}
+                label={item.label}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSwitchSessionUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
   );
 };
 
+// Navbar item conponent
+function NavbarItem({ link, label, handleClick }: { link: string; label: string; handleClick?: () => void }) {
+  const pathName = usePathname();
+  const isActive = pathName === link;
+  return (
+    <div className="relative flex items-center">
+      <Link
+        href={link}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-fulll jsutify-start text-lg text-muted-foreground hover:text-foreground",
+          isActive && "text-foreground"
+        )}
+        onClick={()=>handleClick && handleClick()}
+      >
+        {label}
+      </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block"></div>
+      )}
+    </div>
+  );
+}
 export default Navbar;
